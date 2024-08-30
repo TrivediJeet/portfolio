63d91e3211d78893bc42250db43362c95ff99f74
Jeet <trivedi.jeet@gmail.com>
2024-08-30 12:57:46 -0400

    Update

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -19,8 +19,8 @@ const RecentProjects = () => {
             key={item.id}
           >
             <PinContainer
-              title="/ui.aceternity.com"
-              href="https://twitter.com/mannupaaji"
+              title={item.title}
+              href="https://github.com/TrivediJeet"
             >
               <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
                 <div
@@ -32,7 +32,7 @@ const RecentProjects = () => {
                 <img
                   src={item.img}
                   alt="cover"
-                  className="z-10 absolute bottom-0"
+                  className="z-10 absolute bottom-0 h-full"
                 />
               </div>
 
